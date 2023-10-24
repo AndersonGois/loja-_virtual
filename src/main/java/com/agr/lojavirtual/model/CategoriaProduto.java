1b1c7db12a5efda94970cb132b41f497cf72122a
Anderson <anderson.eed>
2023-10-24 11:43:23 -0300

    Inclusão da classe Categoria Produto, usuario, acesso, endereco, marca produto,e pessoa

--- src/main/java/com/agr/lojavirtual/model/CategoriaProduto.java
+++ src/main/java/com/agr/lojavirtual/model/CategoriaProduto.java
@@ -12,40 +12,35 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable {
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto" , sequenceName = "seq_categoria_produto" ,allocationSize = 1, initialValue = 1)
+public class CategoriaProduto  implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
 	private long id;
 	
-	@Column(name = "nome_desc", nullable = false)
-	private String nomeDescricao;
+	@Column(name = "nome_desc" ,nullable = false)
+	private String nomeDesc;
 
 	public long getId() {
 		return id;
 	}
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
-	public String getNomeDescricao() {
-		return nomeDescricao;
+	public String getNomeDesc() {
+		return nomeDesc;
 	}
-
-	public void setNomeDescricao(String nomeDescricao) {
-		this.nomeDescricao = nomeDescricao;
+	public void setNomeDesc(String nomeDesc) {
+		this.nomeDesc = nomeDesc;
 	}
-
 	@Override
 	public int hashCode() {
-		return Objects.hash(id, nomeDescricao);
+		return Objects.hash(id, nomeDesc);
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -54,8 +49,9 @@ public class MarcaProduto implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		MarcaProduto other = (MarcaProduto) obj;
-		return id == other.id && Objects.equals(nomeDescricao, other.nomeDescricao);
+		CategoriaProduto other = (CategoriaProduto) obj;
+		return id == other.id && Objects.equals(nomeDesc, other.nomeDesc);
 	}
-
+	
+	
 }
